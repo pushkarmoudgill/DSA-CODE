3933e76a5a1754f45dcfc8a51b47b603ec79e2f0
pushkar <pushkarmoudgil01@gmail.com>
2024-08-23 23:01:16 +0530

    apply dp to this code

--- InfyLearning/src/com/dp/CoinChange1.java
+++ InfyLearning/src/com/dp/CoinChange1.java
@@ -1,19 +1,23 @@
 package com.dp;
 
+import java.util.Arrays;
+
 public class CoinChange1 {
 public static void main(String [] args) {
 	int coin [] = {1,2,3};
 	
 	int sum =5;
 	
-	
-	int ans=count(sum,coin,0);
+	int[][] dp = new int[coin.length+ 1][sum + 1];
+    for (int[] row : dp)
+        Arrays.fill(row, -1);
+	int ans=count(sum,coin,0,dp);
 	
 	System.out.println(ans);
 }
 
 
-static int count (int sum,int [] coin,int i) {
+static int count (int sum,int [] coin,int i,int [][] dp) {
 	
 	
 	if(sum==0) {
@@ -23,10 +27,13 @@ static int count (int sum,int [] coin,int i) {
 	if(sum<0 || i==coin.length) {
 		return 0;
 	}
+	if(dp[i][sum]!=-1) {
+		return dp[i][sum];
+	}
 	int cnt=0;
-	 cnt+= count (sum-coin[i],coin,i);
-	cnt+=count(sum,coin,i+1);
-	
-	return cnt;
+	 cnt+= count (sum-coin[i],coin,i,dp);
+	cnt+=count(sum,coin,i+1,dp);
+	dp[i][sum]=cnt;
+	return dp[i][sum];
 }
 }
