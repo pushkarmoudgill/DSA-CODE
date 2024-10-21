1c6816f8a4728399aace1e70144576aba6942349
pushkar <pushkarmoudgil01@gmail.com>
2024-10-21 17:16:27 +0530

    improve time complexity consecutive number problem leetcode 128

--- InfyLearning/src/com/hashing/consecutiveNumber.java
+++ InfyLearning/src/com/hashing/consecutiveNumber.java
@@ -13,16 +13,37 @@ public class consecutiveNumber {
 		arr[i]=sc.nextInt();
 	}
 	Arrays.sort(arr);
+	int max=Integer.MIN_VALUE;
+
 	int conNum=0;
-	for(int i=0;i<n-1;i++) {
+	for(int i=0;i<arr.length-1;i++) {
 		int sub=arr[i+1]-arr[i];
-		if(sub==1) {
+		if(sub==1)  {
 			conNum++;
 		}
+      else  if(sub==0)  {
+			//conNum++;
+		}
+       else  if(sub<0)  {
+			//conNum++;
+		}
 		else {
-			break;
+			///break;
+            max=Math.max(max,conNum+1);
+            conNum=0;
 		}
 	}
-	System.out.println(conNum + 1);
+    if(max<=conNum){
+        //return conNum +1;
+    	System.out.println(conNum +1);
+    }
+else{
+	System.out.println(max);
 }
 }
+}
+//[-8,-4,9,9,4,6,1,-4,-1,6,8]
+
+
+// t.c. : 11
+//-8 -4 9 9 4 6 1 -4 -1 6 8
