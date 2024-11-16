cd80db98752775fc0326dd2b2f4a95afc89d44f5
pushkar <pushkarmoudgil01@gmail.com>
2024-11-16 23:45:41 +0530

    solve 3 sum problem

---
+++ InfyLearning/src/com/hashing/ThreeSum.java
@@ -0,0 +1,22 @@
+package com.hashing;
+
+public class ThreeSum {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		int nums[]= {-1,0,1,2,-1,-4};
+		
+		
+		for(int i=0;i<nums.length;i++) {
+			
+			int sum=0-nums[i];
+			
+			//for
+			
+		}
+
+	}
+
+}
