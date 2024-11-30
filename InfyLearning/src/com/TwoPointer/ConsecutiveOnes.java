0e411d118ccd836396475d043f5c74ff1736a663
pushkar <pushkarmoudgil01@gmail.com>
2024-11-30 22:05:28 +0530

    solve ConsecutiveOnes problems

--- InfyLearning/src/com/TwoPointer/ConsecutiveOnes.java
+++ InfyLearning/src/com/TwoPointer/ConsecutiveOnes.java
@@ -3,10 +3,31 @@ package com.TwoPointer;
 public class ConsecutiveOnes {
 	
 	public static void main(String [] args) {
-		int arr[] = {1,1,0,1,1,1};
+		int arr[] = {1,0,1,1,0,1};
 		
+		int max=0;
+		int count =0;
+		for(int i=0;i<arr.length;i++) {
+			
+			if(arr[i]!=1 ) {
+				max=Math.max(max, count);
+				count=0;
+				
+			}
+			else if(arr[i]==1 && i==arr.length-1) {
+				count++;
+				max=Math.max(max, count);
+				
+			}
+			else {
+				count++;
+			}
+			
+			
+		}
 		
-		
+//		return max;
+		System.out.println(max);
 	}
 
 }
